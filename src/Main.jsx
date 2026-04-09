00b1033da6139b09379ebfcb2d4968b74b274e56
ifemstech <michealfaraday71@gmail.com>
2026-04-09 22:13:34 +0100

    Deploy site

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/ifems-xyz">
     <App />
   </BrowserRouter>
 );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -47,6 +47,8 @@ export default function About() {
           Designed & engineered by <span>IFEMS</span>
         </div>
       </div>
+
+      
     </section>
   );
 }
