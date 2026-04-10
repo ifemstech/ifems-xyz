3e717013c44a39cec57ffcc69f5e6a8085b8d241
ifemstech <michealfaraday71@gmail.com>
2026-04-10 10:09:15 +0100

    remove basename fix

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,14 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// Detect environment automatically
-const basename =
-  window.location.hostname.includes("github.io")
-    ? "/ifems-xyz"
-    : "/";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
